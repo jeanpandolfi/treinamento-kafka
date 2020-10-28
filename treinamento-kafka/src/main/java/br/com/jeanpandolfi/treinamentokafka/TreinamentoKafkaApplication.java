d267b117e1dc49877ee24a20448e5e1bbf729a61
Jean Pandolfi <jean-pandolfi@hotmail.com>
2020-10-28 14:04:31 -0300

    Criação do projeto

---
+++ treinamento-kafka/src/main/java/br/com/jeanpandolfi/treinamentokafka/TreinamentoKafkaApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeanpandolfi.treinamentokafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoKafkaApplication.class, args);
+	}
+
+}
---
+++ treinamento-kafka/src/test/java/br/com/jeanpandolfi/treinamentokafka/TreinamentoKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeanpandolfi.treinamentokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
