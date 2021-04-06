331cf09de1371e1a0afdf7e14c4f751b083c1d7b
Jean Pandolfi <jean.pandolfi@basis.com.br>
2021-04-06 16:14:54 -0300

    Modularizando o projeto separando em microserviços

--- ecommerce/service-email/src/main/java/br/com/jeanpandolfi/EmailService.java
+++ ecommerce/service-email/src/main/java/br/com/jeanpandolfi/EmailService.java
@@ -2,7 +2,6 @@ package br.com.jeanpandolfi;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class EmailService {
--- ecommerce/service-fraud-detector/src/main/java/br/com/jeanpandolfi/FraudeDetectorService.java
+++ ecommerce/service-fraud-detector/src/main/java/br/com/jeanpandolfi/FraudeDetectorService.java
@@ -3,7 +3,6 @@ package br.com.jeanpandolfi;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class FraudeDetectorService {
 
