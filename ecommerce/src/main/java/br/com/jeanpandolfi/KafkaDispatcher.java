4a364beed57bf9c74ee3ff41b119d0cc568301d7
Jean Pandolfi <jean.pandolfi@basis.com.br>
2021-04-05 21:59:31 -0300

    mandando objetos como value da mensagem e criando um serialidor customizado

--- ecommerce/src/main/java/br/com/jeanpandolfi/KafkaDispatcher.java
+++ ecommerce/src/main/java/br/com/jeanpandolfi/KafkaDispatcher.java
@@ -10,11 +10,11 @@ import java.io.Closeable;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
-public class KafkaDispatcher implements Closeable {
-    private final KafkaProducer<String, String> producer;
+public class KafkaDispatcher<T> implements Closeable {
+    private final KafkaProducer<String, T> producer;
 
     public KafkaDispatcher() {
-        this.producer = new KafkaProducer<String, String>(properties());
+        this.producer = new KafkaProducer<String, T>(properties());
     }
 
     private static Properties properties() {
@@ -23,13 +23,13 @@ public class KafkaDispatcher implements Closeable {
         /**@param KEY_SERIALIZER_CLASS_CONFIG indica  qual serializador que vai serializar minhas Keys do producer*/
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         /**@param VALUE_SERIALIZER_CLASS_CONFIG indica  qual serializador que vai serializar meus Values do producer*/
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
 
         return properties;
     }
 
-    public void send(String topic, String key, String value) throws ExecutionException, InterruptedException {
-        var record = new ProducerRecord<String, String>(topic, key, value);
+    public void send(String topic, String key, T value) throws ExecutionException, InterruptedException {
+        var record = new ProducerRecord<String, T>(topic, key, value);
         Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
